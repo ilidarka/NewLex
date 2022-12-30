333b9c457da89104818c4c28fd3fee3573a3248c
Vladi Leshko <leshkovladik@gmail.com>
2022-12-30 12:23:03 +0300

    some adaptive fixes

--- scripts/directorsSelectScript.js
+++ scripts/directorsSelectScript.js
@@ -1,5 +1,8 @@
-let directorItems = document.querySelectorAll(".directorItem")
-let directorContenList = document.querySelectorAll(".left-right-column");
+const directorItems = document.querySelectorAll(".directorItem")
+const directorContenList = document.querySelectorAll(".left-right-column");
+const directorNames = document.querySelectorAll(".directorsName");
+const directorArrowLeft = document.querySelector(".directorArrowLeft");
+const directorArrowRight = document.querySelector(".directorArrowRight");
 
 directorItems.forEach((elem) => {
     elem.addEventListener('click', (event) => changeDirectorItem(event, elem.dataset.btn));
@@ -13,9 +16,27 @@ const changeDirectorItem = (event, buttonNumber) => {
     showContent(buttonNumber);
 };
 
+directorArrowLeft.addEventListener('click', (event) => {
+    let currentContent = document.querySelector(".activeDirectorName");
+    if(Number(currentContent.dataset.directors) > 1) {
+        showContent(Number(currentContent.dataset.directors) - 1)
+    }
+});
+
+directorArrowRight.addEventListener('click', (event) => {
+    let currentContent = document.querySelector(".activeDirectorName");
+    if(Number(currentContent.dataset.directors) < directorContenList.length) {
+        showContent(Number(currentContent.dataset.directors) + 1)
+    }
+});
+
 function showContent(contentNumber) {
     directorContenList.forEach((elem) => {
         elem.classList.add("hideContent");
     });
+    directorNames.forEach((elem) => {
+        elem.classList.remove("activeDirectorName");
+    });
     directorContenList[contentNumber - 1].classList.remove("hideContent");
+    directorNames[contentNumber - 1].classList.add("activeDirectorName");
 }
