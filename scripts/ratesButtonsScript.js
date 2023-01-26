0fce1591c72389dc32ae39f1fe477562e5606c6e
Vladik Leshko <leshkovladik@gmail.com>
2023-01-26 17:46:55 +0300

    some fixes

--- scripts/ratesButtonsScript.js
+++ scripts/ratesButtonsScript.js
@@ -1,12 +1,23 @@
 let buttons = document.querySelectorAll(".rateButton")
+let rates = document.querySelectorAll(".ratesPrices");
 
 buttons.forEach((elem) => {
-    elem.addEventListener('click', (event) => changeButtonColor(event));
+    elem.addEventListener('click', (event) => changeButtonColor(event, elem.dataset.ratebutton));
 });
 
-const changeButtonColor = (event) => {
+const changeButtonColor = (event, buttonNumber) => {
     buttons.forEach((elem) => {
         elem.classList.remove("active");
     });
     event.currentTarget.classList.add("active");
+    changeActiveRate(buttonNumber);
 };
+
+function changeActiveRate(rateNumber) {
+    rates.forEach((elem) => {
+        elem.classList.add("unactiveRate");
+        if(elem.dataset.ratesprices == rateNumber) {
+            elem.classList.remove("unactiveRate");
+        }
+    });
+}
