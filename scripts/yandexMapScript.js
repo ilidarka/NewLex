84fcc7d9cc423b854fd8dbeeda7da051c437a090
Vladik Leshko <leshkovladik@gmail.com>
2023-02-15 13:15:44 +0300

    fixed map

--- scripts/yandexMapScript.js
+++ scripts/yandexMapScript.js
@@ -9,8 +9,8 @@ function init () {
     myMap = new ymaps.Map('map', {
         // При инициализации карты обязательно нужно указать
         // её центр и коэффициент масштабирования.
-        center: [55.76, 37.64], // Москва
-        zoom: 10
+        center: [55.655193, 37.533607], // Москва
+        zoom: 17
     }, {
         searchControlProvider: 'yandex#search'
     });
