02f1d47ac15158f6fd4870c3d5f9c8e03cb06e66
Vladik Leshko <leshkovladik@gmail.com>
2023-03-21 12:13:33 +0300

    directors work

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,41 +1,15 @@
-var $swiperSelector = $('.swiper-container');
+window.onload = function() {
+  renderSlider();
+}
 
-$swiperSelector.each(function(index) {
-    var $this = $(this);
-    $this.addClass('swiper-slider-' + index);
-    
-    var dragSize = $this.data('drag-size') ? $this.data('drag-size') : 50;
-    var freeMode = $this.data('free-mode') ? $this.data('free-mode') : false;
-    var loop = $this.data('loop') ? $this.data('loop') : false;
-    var slidesDesktop = $this.data('slides-desktop') ? $this.data('slides-desktop') : 2;
-    var slidesTablet = $this.data('slides-tablet') ? $this.data('slides-tablet') : 2;
-    var slidesMobile = $this.data('slides-mobile') ? $this.data('slides-mobile') : 1;
-    var spaceBetween = $this.data('space-between') ? $this.data('space-between'): 20;
-
-    var swiper = new Swiper('.swiper-slider-' + index, {
+function renderSlider() {
+  var swiper = new Swiper('.swiper', {
       direction: 'horizontal',
-      loop: loop,
-      freeMode: freeMode,
-      spaceBetween: spaceBetween,
-      breakpoints: {
-        1920: {
-          slidesPerView: slidesDesktop
-        },
-        992: {
-          slidesPerView: slidesTablet
-        },
-        400: {
-           slidesPerView: slidesMobile
-        }
-      },
+      loop: false,
+      spaceBetween: 20,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
       },
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
-        dragSize: dragSize
-      }
    });
-});
+}
